bafd8348b521f2f4f763e594b11ef57ef3ca5bbf
muschellij2 <muschellij2@gmail.com>
2016-09-15 13:16:28 -0400

    updating ids

--- R/get_ids.R
+++ R/get_ids.R
@@ -1,14 +1,22 @@
-#' @name get_ids
-#' @title Get a list of the IDs
+#' @title Get IDs with Data in Package
 #'
-#' @description Return the IDs for the people scanned
+#' @description Return the IDs for the people scanned available for reading
+#' 
 #' @export
 #' @importFrom utils data
 get_ids = function(){
   # kirby21_demog = NULL
   # rm(list = "kirby21_demog")
   # 
+  id_visit_1 = list.dirs(system.file("visit_1", package = "kirby21"), 
+            recursive = FALSE, 
+            full.names = FALSE)
+  id_visit_2 = list.dirs(system.file("visit_1", package = "kirby21"), 
+                         recursive = FALSE, 
+                         full.names = FALSE)  
+  ids = as.numeric(c(id_visit_1, id_visit_2))
+  ids = unique(ids)
   # data("kirby21_demog", envir = environment())
-  ids = c(113, 505, 849)
+  # ids = c(113, 505)
   return(ids)
 }
