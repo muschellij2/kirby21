ea64d7e502d19169ade3e9c9b621c2a10041519f
muschellij2 <muschellij2@gmail.com>
2016-09-15 13:36:43 -0400

    Added subject_id_to_visit_id to export.

---
+++ R/subject_id_to_visit_id.R
@@ -0,0 +1,9 @@
+#' @name subject_id_to_visit_id
+#' @title Kirby 21 Subject Identifiers to NITRC Visit Identifiers
+#'
+#' @description A dataset containing the mapping from the Subject IDs from the 
+#' Kirby demographics to the KKI2009 idenifiers on NITRC
+#' @format A data frame with 42 rows and 4 columns 
+#'
+#' @source \url{https://www.nitrc.org/frs/?group_id=313}
+NULL
